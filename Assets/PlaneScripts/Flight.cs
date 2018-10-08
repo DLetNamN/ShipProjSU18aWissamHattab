47c2740aace52f80d1d7ff8b00759a93ee5bacb5
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2018-10-08 10:41:51 +0200

    Putting stuff into folders
    Oke

--- Assets/PlaneScripts/Flight.cs
+++ Assets/PlaneScripts/Flight.cs
@@ -7,7 +7,7 @@ public class Flight : MonoBehaviour
 
     private float moveSpeed = 1f; //moveSpeed variable.
     private float turnSpeed = 45f; //Turn/rotaion speed variable.
-    private float leftTurnSpeed = 22.5f;
+    private float leftTurnSpeed = 22.5f; //I could do (turnSpeed / 2), or just separate the different sides from the beginning.
     
 
 
@@ -27,11 +27,11 @@ public class Flight : MonoBehaviour
             transform.Rotate(new Vector3(0, 0, leftTurnSpeed) * Time.deltaTime); //Makes the vehicle turn, this time the rotation isn't in the negative, so as to turn the opposite direction.
         }
 
-        transform.Translate(0, 1 * Time.deltaTime, 0); //Keeps the ship moving/
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0); //Keeps the ship moving/
 
         if (Input.GetKey(KeyCode.S)) //Buttonpress to slow down.
         {
-            transform.Translate(0, -0.5f * Time.deltaTime, 0); //Since the movement is constant, I just have to subtract half of the speed.
+            transform.Translate(0, (moveSpeed / 2) * Time.deltaTime, 0); //Since the movement is constant, I just have to divide the speed by half (or by a third, or a fourth, etc etc.).
         }
     }
 
