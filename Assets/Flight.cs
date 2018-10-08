e33cdf273dac116e7c08b2f1a937eb5bca255add
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2018-10-08 09:03:40 +0200

    ColorWorks

--- Assets/Flight.cs
+++ Assets/Flight.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flight : MonoBehaviour {
+public class Flight : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float moveSpeed = 1;
+    public float turnSpeed = 45f;
+    private SpriteRenderer PlaneSprite;
+    private Color planeColor;
+
+
+    void movement()
+    {
+
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(new Vector3(0, 0, -turnSpeed) * Time.deltaTime);
+            PlaneSprite.color = new Color(1f, 1f, 10f);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
+
+        }
+        transform.Translate(0, 1f * Time.deltaTime, 0);
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+
+    }
 }
---
+++ Assets/PlaneColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneColor : MonoBehaviour
+{
+
+    public SpriteRenderer PlaneSprite;
+    public Color planeColor;
+
+    void ColorChanger()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlaneSprite.color = new Color(0f, 0f, 10f);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorChanger();
+    }
+}
